02755173e0affe836bab6e6c6f95385104b2cd5d
SavageBillyBoy <teamozkee@gmail.com>
2025-06-11 13:59:24 -0400

    m

--- javascripts/transition.js
+++ javascripts/transition.js
@@ -7,6 +7,13 @@ window.onload = function() {
     }, 250)
 };
 
+window.addEventListener('load', function () {
+  setTimeout(() => {
+        document.getElementById("transition-cover").classList.add("faded");
+        console.log(document.getElementById("transition-cover").classList);
+    }, 250)
+})
+
 
 function startfadeOut(link) {
     document.getElementById("transition-cover").classList.remove("faded");
