e07504c5c6d20800d158030a2fc1472d94f8dffc
SavageBillyBoy <teamozkee@gmail.com>
2025-06-11 14:04:12 -0400

    m

--- javascripts/transition.js
+++ javascripts/transition.js
@@ -1,15 +1,19 @@
 const duration = 1;
 
-window.onload = function() {
-    setTimeout(() => {
-        document.getElementById("transition-cover").classList.add("faded");
-        console.log(document.getElementById("transition-cover").classList);
-    }, 250)
-};
-
-window.addEventListener('load', function () {
-  alert("It's loaded!")
-})
+window.addEventListener('pageshow', function(event) {
+        if (event.persisted) {
+            setTimeout(() => {
+                document.getElementById("transition-cover").classList.add("faded");
+                console.log(document.getElementById("transition-cover").classList);
+            }, 250);
+        } else {
+            // Code to execute when the page is loaded normally
+            setTimeout(() => {
+                document.getElementById("transition-cover").classList.add("faded");
+                console.log(document.getElementById("transition-cover").classList);
+            }, 250);
+        }
+    });
 
 
 function startfadeOut(link) {
